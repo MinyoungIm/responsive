4201439de68dae683bd738a6fd930d66addeea3d
MinyoungIm <86937685+MinyoungIm@users.noreply.github.com>
2021-09-29 14:47:51 +0900

    프로젝트 업데이트

---
+++ js/script.js
@@ -0,0 +1,53 @@
+﻿$(function(){
+	/* gnb menu */
+	
+	$('.gnb>li').hover(function(){
+		$(this).find('.subWrapBg').stop().slideDown();
+	},function(){
+		$(this).find('.subWrapBg').stop().slideUp();
+	}); 
+	
+	/* gnb scroll*/
+	/* $(window).scroll(function(){
+		
+	}); */
+	
+	$(window).on('scroll',function(){
+	
+	
+	
+			var sct=$(window).scrollTop();
+		
+		if(sct>=30){
+			$('.headerTop').stop().animate({height:0},100);
+			$('.gnbWrap').stop().animate({top:0},100);
+			$('.subWrapBg').stop().animate({top:'70px'},100);
+		}else if(sct<=30){
+			$('.headerTop').stop().animate({height:'29px'},200);
+			$('.gnbWrap').stop().animate({top:'30px'},200);
+			$('.subWrapBg').stop().animate({top:'70px'},200);
+		}
+		
+		console.log(innerWidth);
+		
+		if(sct > 30 && innerWidth > 750 && innerWidth < 911) {
+			
+			$('.headerTop').stop().animate({height:0},200);
+			$('.gnbWrap').stop().animate({top:0},200);
+			$('.subWrapBg').stop().animate({top:'120px'},200);
+			
+			console.log($('.subWrapBg').top);
+			console.log('test1');
+			
+		} else if(sct <= 30 && innerWidth < 911) {
+		
+			$('.headerTop').stop().animate({height:0},200);
+			$('.gnbWrap').stop().animate({top:0},200);
+			$('.subWrapBg').stop().animate({top:'120px'},200);
+			
+			console.log($('.subWrapBg').top);
+			console.log('test2');
+		
+		}
+		});
+});
